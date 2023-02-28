ed42a53148d93301e5109e116838f42fccd3d5c9
Glitch (onyx-scythe-scaffold) <none>
2023-02-28 00:48:36 +0000

    📊🕜 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,16 @@
+// Write the code, one line for each action:
+
+// Create an empty object user.
+let user = {
+
+// Add the property name with the value John.
+  name: "John",
+
+// Add the property surname with the value Smith.
+  surname: "Smith",
+    
+// Change the value of the name to Pete.
+  name: "Pete",
+}
+// Remove the property name from the object.
+  delete(user.name);
